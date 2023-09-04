1805e8ef46041d7eaead7d3e0f954e1c02a15249
Beatriz Andrade <b.dandrade@hotmail.com>
2023-09-04 11:16:15 -0300

    Add files via upload

--- ProjetoServlets/src/main/java/servlets/ExUm.java
+++ ProjetoServlets/src/main/java/servlets/ExUm.java
@@ -5,17 +5,28 @@ import jakarta.servlet.annotation.WebServlet;
 import jakarta.servlet.http.HttpServlet;
 import jakarta.servlet.http.HttpServletRequest;
 import jakarta.servlet.http.HttpServletResponse;
+import model.Pessoa;
 
 import java.io.IOException;
 import java.io.PrintWriter;
 
-@WebServlet(urlPatterns = {"/oi", "/bemvindo"})
+@WebServlet
 public class ExUm extends HttpServlet {
     @Override
     protected void service(HttpServletRequest req, HttpServletResponse resp) throws ServletException, IOException{
-//      resp.sendRedirect("testeRedirect.html");
-        String nome = req.getParameter("nome");
+        String nome = req.getParameter("name");
+        String email = req.getParameter("mail");
+        String idade = req.getParameter("age");
+
+        int idadeConvert = Integer.parseInt(idade);
+
+        Pessoa pessoa = new Pessoa(nome, email, idadeConvert);
+
         PrintWriter writer = resp.getWriter();
-        writer.println("<p>Oi " + nome + ", tudo bem?</p>");
+        writer.println("<h1>Seu cadastro</h1>");
+        writer.println("<p>Nome: " + pessoa.getNome() + "</p>");
+        writer.println("<p>Email: " + pessoa.getEmail() + "</p>");
+        writer.println("<p>Idade: " + pessoa.getIdade() + "</p>");
     }
 }
+
--- ProjetoServlets/src/main/webapp/index.jsp
+++ ProjetoServlets/src/main/webapp/index.jsp
@@ -1,5 +1,30 @@
-<html>
+<!DOCTYPE html>
+<html lang="en">
+<head>
+    <meta charset="UTF-8">
+    <meta http-equiv="X-UA-Compatible" content="IE=edge">
+    <meta name="viewport" content="width=device-width, initial-scale=1.0">
+    <title>Document</title>
+</head>
 <body>
-<h2>Hello World!</h2>
+<h1>Formulário de cadastro!</h1>
+<form action="/exemploum" method="post">
+    <div>
+        <label>Nome:</label>
+        <input type="text" name="name"/>
+    </div>
+    <div>
+        <label>E-mail:</label>
+        <input type="email" name="mail"/>
+    </div>
+    <div>
+        <label>Idade:</label>
+        <input type="number" name="age"/>
+    </div>
+    <br>
+    <div class="button">
+        <button type="submit">Enviar seu cadastro</button>
+    </div>
+</form>
 </body>
 </html>
